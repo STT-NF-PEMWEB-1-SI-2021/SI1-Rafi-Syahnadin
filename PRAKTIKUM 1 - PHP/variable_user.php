c83229ae502ba2969b4f0195f0079abc93d6795e
Rafi Syahnadin Akmal <92433879+rafisyahnadin10@users.noreply.github.com>
2022-02-28 21:16:57 +0700

    Add files via upload

---
+++ PRAKTIKUM 1 - PHP/variable_user.php
@@ -0,0 +1,13 @@
+<?php
+    // mendefinisikan variable
+    $nama = 'Rafi Syahnadin Akmal';
+    $umur = 19;
+    $berat = 65;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur. ' Tahun';
+    echo '<br/> Berat : ' . $berat. ' Kg';
+
+    echo "<br/><br/>Hello $nama, apa kabar?";
+
+?>
